f5518f3d57f7cf33f767991f64e994e8bc459e01
Nasaruddin <myselfnasaruddin@gmail.com>
2025-04-04 16:44:43 +0530

    in assets tags

--- app/Models/AssetModel.php
+++ app/Models/AssetModel.php
@@ -9,7 +9,7 @@ class AssetModel extends Model
     protected $table      = 'assets';
     protected $primaryKey = 'id';
 
-    protected $allowedFields = ['title', 'file', 'details', 'room_id', 'style_id', 'created_at', 'updated_at'];
+    protected $allowedFields = ['title', 'tags', 'file', 'details', 'room_id', 'style_id', 'created_at', 'updated_at'];
 
     protected $useTimestamps = true;
     protected $createdField  = 'created_at';
