c7d3fcf109acd9208bb2361226143b93ff3f640f
Nasaruddin <myselfnasaruddin@gmail.com>
2025-04-14 11:53:33 +0530

    service-type modal

--- app/Models/ServiceTypeModel.php
+++ app/Models/ServiceTypeModel.php
@@ -9,7 +9,7 @@ class ServiceTypeModel extends Model
     protected $table      = 'service_types'; // Table name
     protected $primaryKey = 'id'; // Primary key
 
-    protected $allowedFields = ['name', 'image']; // Columns that can be modified
+    protected $allowedFields = ['name', 'image', 'status']; // Columns that can be modified
 
     protected $useTimestamps = true; // Enables created_at and updated_at auto-fill
 }
