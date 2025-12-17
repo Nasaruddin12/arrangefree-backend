9ec6803863ea3d57bc3ff964dddffd114fba630e
Nasaruddin <myselfnasaruddin@gmail.com>
2025-12-17 11:15:44 +0530

    location update

--- app/Controllers/LocationController.php
+++ app/Controllers/LocationController.php
@@ -46,9 +46,10 @@ class LocationController extends ResourceController
 
         $client = \Config\Services::curlrequest([
             'curloptions' => [
-                CURLOPT_IPRESOLVE => CURL_IPRESOLVE_V4
+                CURLOPT_IPRESOLVE => CURL_IPRESOLVE_V4,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             ],
-            'timeout' => 10
+            'timeout' => 10,
         ]);
 
         $response = $client->get('https://ifconfig.me');
