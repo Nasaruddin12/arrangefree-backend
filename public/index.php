5d42af7cfb052cea10c573010f4536b0f818515a
Nasaruddin <myselfnasaruddin@gmail.com>
2025-12-27 14:30:16 +0530

    index

--- public/index.php
+++ public/index.php
@@ -12,7 +12,14 @@ if (version_compare(PHP_VERSION, $minPhpVersion, '<')) {
     exit($message);
 }
 
-header("Access-Control-Allow-Origin: http://localhost:5173");
+$allowedOrigins = [
+    'https://localhost:3000',
+    'http://localhost:5173',
+];
+
+if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
+    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+}
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
